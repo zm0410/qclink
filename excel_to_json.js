8df7c9b4e7a1e87bd3d7590a355a2ef2a0ed764b
zheler <61550111+zm0410@users.noreply.github.com>
2025-09-24 17:07:27 +0800

    Delete excel_to_json.js

--- excel_to_json.js
+++
@@ -1,9 +0,0 @@
-const XLSX = require('xlsx');
-const fs = require('fs');
-
-const workbook = XLSX.readFile('data.xlsx');
-const sheet_name_list = workbook.SheetNames;
-const sheet = workbook.Sheets[sheet_name_list[0]];
-const json_data = XLSX.utils.sheet_to_json(sheet);
-
-fs.writeFileSync('data.json', JSON.stringify(json_data, null, 2));
